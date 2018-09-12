a98bf9ed8104766affab631a75892807551495b1
Patrick Thomas Ørum <Patr421b@edu.easj.dk>
2018-09-12 15:24:58 +0200

    created

---
+++ Math/Calculus/Math.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math
+{
+    public class Math
+    {
+        /// <summary>
+        /// adds 2 int values: a + b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        /// <summary>
+        /// Subtracts 2 int values: a - b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        /// <summary>
+        /// Divides 2 values: a / b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+        /// <summary>
+        /// Multiplies 2 int values: a * b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        
+    }
+}
